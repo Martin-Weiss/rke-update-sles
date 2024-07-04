597caea290c983ce85f467eec6fb72a431a0a540
root <martin.weiss@suse.com>
2024-07-04 16:47:33 +0200

    few updates

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #kubectl uncordon -l kubernetes.io/os=linux
-kubectl label ns rke-update-sles pod-security.kubernetes.io/enforce=permissive --overwrite
+kubectl label ns rke-update-sles pod-security.kubernetes.io/enforce=privileged --overwrite
 kubectl apply -f system-upgrade-controller.yaml
 sleep 30
 kubectl apply -f rke-update-sles.yaml
